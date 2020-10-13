76e455f7ecef670934ea97695056b628f79f6a68
Lv Jie <lvjie@jurassic.com.cn>
2020-10-13 22:05:56 +0800

    开始学习

---
+++ src/main/java/com/example/rokey/SpringCloudRootApplication.java
@@ -0,0 +1,13 @@
+package com.example.rokey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudRootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rokey/SpringCloudRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rokey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
